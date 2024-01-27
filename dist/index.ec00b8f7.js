371daca02d3f78ae82ca16d5100b72a7adeae754
Gustav Brodin <brodin.gustav@gmail.com>
2024-01-27 23:29:56 +0100

    Kört npm run build med lyckat resultat.

---
+++ dist/index.ec00b8f7.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("open-menu"),n=document.getElementById("close-menu");function t(){let e=document.getElementById("nav-menu");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",t),n.addEventListener("click",t);
+//# sourceMappingURL=index.ec00b8f7.js.map
